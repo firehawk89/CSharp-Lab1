aa92c1ff4a382a19538dbc330a85ced10f40d2bf
Bochkovskyi <unundertale1@ukr.net>
2022-10-05 21:19:48 +0300

    Modified Program class

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Lab1;
+
+GameAccount firstPlayer = new GameAccount("Anton");
+GameAccount secondPlayer = new GameAccount("Ivan");
+
+firstPlayer.WinGame(secondPlayer, DateTime.Now, 5);
+
+/*
+try
+{
+    firstPlayer.LoseGame(secondPlayer, DateTime.Now, 15);
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("The Player's rating is too low for this game!");
+    Console.WriteLine(e.ToString());
+    return;
+}
+*/
+
+firstPlayer.LoseGame(secondPlayer, DateTime.Now, 7);
+firstPlayer.WinGame( secondPlayer, DateTime.Now, 4);
+firstPlayer.WinGame(secondPlayer, DateTime.Now, 1);
+
+secondPlayer.WinGame(firstPlayer, DateTime.Now, 3);
+secondPlayer.WinGame(firstPlayer, DateTime.Now, 1);
+secondPlayer.WinGame(firstPlayer, DateTime.Now, 6);
+secondPlayer.LoseGame(firstPlayer, DateTime.Now, 4);
+secondPlayer.LoseGame(firstPlayer, DateTime.Now, 8);
+
+Console.WriteLine(firstPlayer.GetStats());
+Console.WriteLine(secondPlayer.GetStats());
+Console.WriteLine($"{firstPlayer.UserName}'s Rating: " + firstPlayer.CurrentRating);
+Console.WriteLine($"{secondPlayer.UserName}'s Rating: " + secondPlayer.CurrentRating);

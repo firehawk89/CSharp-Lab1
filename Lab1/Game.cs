401125da70224aedeffd21e20d309b84f3733177
Bochkovskyi <unundertale1@ukr.net>
2022-10-05 13:37:40 +0300

    Added Game class

---
+++ Lab1/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Game
+    {
+        public string Player { get; }
+        public string Opponent { get;  }
+        public DateTime Date { get;  }
+        public int Rating { get; }
+
+
+        public Game(string Player, string Opponent, DateTime Date, int Rating)
+        {
+            this.Player = Player;
+            this.Opponent = Opponent;
+            this.Date = Date;
+            this.Rating = Rating;
+        }
+    }
+}
